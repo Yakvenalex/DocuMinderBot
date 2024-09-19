1576505122ebf11eca116e500071ff6d8d57eef9
Yakovenko Oleksii <mr.mnogo@gmail.com>
2024-09-19 16:01:47 +0300

    fin project

--- create_bot.py
+++ create_bot.py
@@ -1,4 +1,3 @@
-import os
 import logging
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
@@ -9,9 +8,6 @@ from decouple import config
 admins = [int(admin_id) for admin_id in config('ADMINS').split(',')]
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
-
-script_dir = os.path.dirname(os.path.abspath(__file__))
-db_file = os.path.join(script_dir, 'bot.db')
 
 bot = Bot(token=config('TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher(storage=MemoryStorage())
